c4d1249a71df2554b171f0021dcf154b88778932
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2018-09-28 11:54:36 +0200

    stage 6: HTML with empty Head

--- src/org/bonitasoft/livecoding/kotlin/dsl/program.kt
+++ src/org/bonitasoft/livecoding/kotlin/dsl/program.kt
@@ -1,15 +1,27 @@
 package org.bonitasoft.livecoding.kotlin.dsl
 
 
-
 fun main(args: Array<String>) {
 
     html {
-
+        head { }
     }
 
 }
 
-fun html (init: () -> Unit){
+fun html(init: HTML.() -> Unit): HTML {
+    val html = HTML()
+    html.init()
+    return html
+}
+
+class HTML {
+    fun head(init: () -> Unit): Head {
+
+    }
+}
+
+class Head {
+
 }
 
