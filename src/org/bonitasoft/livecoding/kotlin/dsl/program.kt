98aa5a809cd88861198867cb84c3d1d620bac298
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2018-09-28 11:46:47 +0200

    stage 5: empty HTML

--- src/org/bonitasoft/livecoding/kotlin/dsl/program.kt
+++ src/org/bonitasoft/livecoding/kotlin/dsl/program.kt
@@ -1,42 +1,15 @@
 package org.bonitasoft.livecoding.kotlin.dsl
 
 
-fun callFunctionOnString(maFunction: () -> Unit) = maFunction()
-
 
 fun main(args: Array<String>) {
 
-    callFunctionOnString {
-        println("Live coding")
-    }
+    html {
 
+    }
 
 }
 
+fun html (init: () -> Unit){
+}
 
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//fun callFunctionOnString(message: String, maFonction: (obj: String) -> Int): Int = maFonction(message)
-//
-//
-//fun main(args: Array<String>) {
-//    val returnCode = callFunctionOnString("coucou") { message ->
-//        println(message)
-//        0
-//    }
-//
-//    println(returnCode)
-//}
